991d4afdd5e5004b262331a74f709ce41dcf1f52
Simon Woolf <simon@ably.io>
2018-03-18 17:21:39 +0100

    Add basic snakepart drawing

--- snake.elm
+++ snake.elm
@@ -6,19 +6,39 @@ import Html.Events exposing (onClick, onInput)
 import Keyboard exposing (KeyCode)
 import Char
 import Time
-import Collage exposing (filled, square)
+import Collage exposing (filled, square, move)
 import Element
-import Color exposing (black)
-
+import Color exposing (black, rgb)
 
 main = Html.program { init = init, view = view, update = update, subscriptions = subscriptions }
 
 
+-- constants
+-- Coordinate system: origin in the center, increasing up (y) and right (x).
+-- Want snake to start in the center at 0,0, so make boardSize an odd number
+
+boardSize : Int
+boardSize = 21
+
+gameSize : Int
+gameSize = 500
+
+useableGameSize : Int
+useableGameSize = 480
+
+snakePartSize : Float
+snakePartSize = (toFloat useableGameSize) / (toFloat boardSize)
+
+snakePartVisibleSize : Float
+snakePartVisibleSize = snakePartSize - 2
+
+yellow : Color.Color
+yellow = Color.rgb 249 161 30
+
+
 -- Types & model
 
-
 type alias Model = { snake : List ( Int, Int ), fruit : Maybe ( Int, Int ), lastDirection : InputEnum, lastTick : Maybe Time.Time }
-
 
 type InputEnum
     = Up
@@ -27,18 +47,21 @@ type InputEnum
     | Right
     | StartStop
 
-
 type Msg
     = Input InputEnum
     | Tick Time.Time
 
-
 init : ( Model, Cmd Msg )
-init = ( Model [] Nothing StartStop Nothing, Cmd.none )
+init = ( { snake = [ ( 1, 1 ), ( 2, 2 ), ( 3, 3 ), ( 10, 10 ) ]
+      , fruit = Nothing
+      , lastDirection = StartStop
+      , lastTick = Nothing
+      }
+    , Cmd.none
+    )
 
 
 -- Updates and subscriptions
-
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model = case msg of
@@ -46,13 +69,11 @@ update msg model = case msg of
 
         Tick time -> ( { model | lastTick = Just time }, Cmd.none )
 
-
 subscriptions : Model -> Sub Msg
 subscriptions model = Sub.batch
         [ Keyboard.downs parseKeyCode
         , Time.every Time.second Tick
         ]
-
 
 parseKeyCode : KeyCode -> Msg
 parseKeyCode code = -- todo make it a maybe input, so can ignore if not recognised
@@ -94,21 +115,24 @@ parseKeyCode code = -- todo make it a maybe input, so can ignore if not recognis
 
 -- Graphics
 
+background : List Collage.Form
+background = [ filled black (square (toFloat gameSize)) ]
 
-gameSize : Int
-gameSize = 500
+setSnakePartPosition : Int -> Int -> Collage.Form -> Collage.Form
+setSnakePartPosition x y = move ( (toFloat x) * snakePartSize, (toFloat y) * snakePartSize )
 
+snakePart : ( Int, Int ) -> Collage.Form
+snakePart ( x, y ) = filled yellow (square snakePartVisibleSize)
+        |> setSnakePartPosition x y
 
-background : Collage.Form
-background = filled black (square (toFloat gameSize))
+snake : Model -> List Collage.Form
+snake model = List.map snakePart model.snake
 
-
-canvas : model -> Element.Element
-canvas model = Collage.collage gameSize gameSize [ background ]
+canvas : Model -> Element.Element
+canvas model = Collage.collage gameSize gameSize (background ++ (snake model))
 
 
 -- View
-
 
 view : Model -> Html Msg
 view model = Html.body []
